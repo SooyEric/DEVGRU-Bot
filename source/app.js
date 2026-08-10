e23572b6809e132b9a09a8a872aacd3cf45b8529
SooyEric <soooyeric@gmail.com>
2026-08-09 19:44:26 -0600

    Update app.js

--- source/app.js
+++ source/app.js
@@ -1,1 +1,9 @@
+import { Client, GatewayIntentBits } from 'discord.js';
 
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds
+  ]
+});
+
+client.login(process.env.DISCORD_TOKEN);
