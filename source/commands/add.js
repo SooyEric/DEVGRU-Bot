4f2bf21a56726971ad7b6f7b9ef6cc9fd476f699
SooyEric <soooyeric@gmail.com>
2026-08-09 23:11:55 -0600

    Update add.js

--- source/commands/add.js
+++ source/commands/add.js
@@ -24,19 +24,25 @@ const RED_ROLES = [
     "1373365867576033440"
 ];
 
-const BLUE_EXCLUSIVE_ROLES = [
-    "1373365805693009920",
-    "1373365858784514241",
-    "1373365868569952298"
-];
-
-const RED_EXCLUSIVE_ROLES = [
-    "1373365804279791839",
-    "1373365857928876243",
-    "1373365867576033440"
-];
-
 const GUEST_ROLE = "1373365890623602768";
+
+const SHARED_ROLES = [
+    "1373365810910859265",
+    "1373365831454556312",
+    "1373365832914178179",
+    "1373365840677703865",
+    "1373365856524046488",
+    "1373365865734738070",
+    "1373365866657222819"
+];
+
+const BLUE_EXCLUSIVE_ROLES = BLUE_ROLES.filter(
+    roleId => !SHARED_ROLES.includes(roleId)
+);
+
+const RED_EXCLUSIVE_ROLES = RED_ROLES.filter(
+    roleId => !SHARED_ROLES.includes(roleId)
+);
 
 export default {
     name: "add",
@@ -75,30 +81,28 @@ export default {
             if (group === "blue") {
                 await member.roles.remove(RED_EXCLUSIVE_ROLES);
                 await member.roles.remove(GUEST_ROLE);
+
                 await member.roles.add(BLUE_ROLES);
+
                 await member.setNickname("SOE1 Bravo");
             }
 
             if (group === "red") {
                 await member.roles.remove(BLUE_EXCLUSIVE_ROLES);
                 await member.roles.remove(GUEST_ROLE);
+
                 await member.roles.add(RED_ROLES);
+
                 await member.setNickname("SOE1 Alpha");
             }
 
             if (group === "guest") {
-                await member.roles.remove(BLUE_EXCLUSIVE_ROLES);
-                await member.roles.remove(RED_EXCLUSIVE_ROLES);
-                await member.roles.remove([
-                    "1373365810910859265",
-                    "1373365831454556312",
-                    "1373365832914178179",
-                    "1373365840677703865",
-                    "1373365856524046488",
-                    "1373365865734738070",
-                    "1373365866657222819"
-                ]);
+                await member.roles.remove(BLUE_ROLES);
+                await member.roles.remove(RED_ROLES);
+                await member.roles.remove(GUEST_ROLE);
+
                 await member.roles.add(GUEST_ROLE);
+
                 await member.setNickname(null);
             }
 
