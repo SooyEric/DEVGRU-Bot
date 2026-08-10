c358b458e0c2970fa128938760623422ef966a83
SooyEric <soooyeric@gmail.com>
2026-08-09 19:50:42 -0600

    Update app.js

--- source/app.js
+++ source/app.js
@@ -1,9 +1,18 @@
-import { Client, GatewayIntentBits } from 'discord.js';
-
+import { Client, GatewayIntentBits } from "discord.js";
+import config from "./config/config.js";
+import registerEvents from "./events/index.js";
+import logger from "./utils/logger.js";
+if (!config.discord.token) {
+    logger.error("DISCORD_TOKEN is not configured.");
+    process.exit(1);
+}
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds
-  ]
+    intents: [
+        GatewayIntentBits.Guilds
+    ]
 });
-
-client.login(process.env.DISCORD_TOKEN);
+registerEvents(client);
+client.login(config.discord.token).catch((error) => {
+    logger.error("Failed to login to Discord:", error);
+    process.exit(1);
+});
