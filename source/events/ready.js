7ba82b68554365f25b5c4807327184d37e09b522
SooyEric <soooyeric@gmail.com>
2026-08-09 19:51:12 -0600

    Update ready.js

--- source/events/ready.js
+++ source/events/ready.js
@@ -1,1 +1,8 @@
-
+import { Events } from "discord.js";
+import logger from "../utils/logger.js";
+export default function ready(client) {
+    client.once(Events.ClientReady, (readyClient) => {
+        logger.info(`Logged in as ${readyClient.user.tag}`);
+        logger.info("DEVGRU-Bot is online.");
+    });
+}
