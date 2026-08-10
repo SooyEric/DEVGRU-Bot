a2915912566f5ba3fc82842aa81aa5edeb080b6b
SooyEric <soooyeric@gmail.com>
2026-08-09 19:50:12 -0600

    Update config.js

--- source/config/config.js
+++ source/config/config.js
@@ -1,1 +1,11 @@
-
+const config = {
+    discord: {
+        token: process.env.DISCORD_TOKEN,
+        clientId: process.env.DISCORD_CLIENT_ID
+    },
+    database: {
+        url: process.env.DATABASE_URL
+    },
+    environment: process.env.NODE_ENV || "development"
+};
+export default config;
