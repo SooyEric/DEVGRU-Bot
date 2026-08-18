88e94436878ed41fd3247192d9d8bb70957f4c84
SooyEric <soooyeric@gmail.com>
2026-08-18 01:49:55 -0600

    Update robloxProfileOAuth.js

--- source/utils/robloxProfileOAuth.js
+++ source/utils/robloxProfileOAuth.js
@@ -63,7 +63,7 @@ export function createProfileRobloxAuthorization(
                 "code",
 
             scope:
-                "openid profile",
+                "openid profile group:read",
 
             state
         });
